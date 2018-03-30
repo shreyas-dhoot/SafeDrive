fe90e508cebbdfb0604c8359f68074c355407da4
AdityaJ97 <aj7744904623@gmail.com>
2018-03-30 20:29:37 +0530

    Added new activity for simulation.(demo.java, activity_demo.xml)

--- app/src/main/java/com/example/android/myapplication/Navigate.java
+++ app/src/main/java/com/example/android/myapplication/Navigate.java
@@ -19,7 +19,6 @@ public class Navigate extends AppCompatActivity{
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.navigate);
-        calldistance();
 
     }
     String vehicle = null;
@@ -79,31 +78,6 @@ public class Navigate extends AppCompatActivity{
                     break;
         }
     }
-    public static final String TAG ="DIST";
-    public  void calldistance () {
-        int i;
-        for( i = 0; i < latlon.length - 1; i++) {
-            double d = distance(latlon[i][0], latlon[i][1], latlon[i + 1][0], latlon[i + 1][1]);
-            Toast.makeText(this, "Distance = " + String.valueOf(d), Toast.LENGTH_SHORT).show();
 
-        }
-    }
-    private static double distance(double lat1, double lon1, double lat2, double lon2) {
-        double theta = lon1 - lon2;
-        double dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
-        dist = Math.acos(dist);
-        dist = rad2deg(dist);
-        dist = dist * 60 * 1.1515 * 1.609344;
-
-        return (dist);
-    }
-
-    private static double deg2rad(double deg) {
-        return (deg * Math.PI / 180.0);
-    }
-
-    private static double rad2deg(double rad) {
-        return (rad * 180 / Math.PI);
-    }
 
 }
