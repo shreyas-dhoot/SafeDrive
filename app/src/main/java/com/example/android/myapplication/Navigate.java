db7f96bd264f15c67f4ae7df78bf735f5c20552f
AdityaJ97 <aj7744904623@gmail.com>
2018-03-30 11:57:36 +0530

    OnClick added for Card. Alert box after onclick for navigation

---
+++ app/src/main/java/com/example/android/myapplication/Navigate.java
@@ -0,0 +1,19 @@
+package com.example.android.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by aditya on 30/3/18.
+ */
+
+public class Navigate extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.navigate);
+
+
+    }
+}
