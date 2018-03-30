190815e8816ad033d9c747c0a6750e95f470079a
AdityaJ97 <aj7744904623@gmail.com>
2018-03-30 12:40:08 +0530

    added radio boxes for demo code

--- app/src/main/java/com/example/android/myapplication/Navigate.java
+++ app/src/main/java/com/example/android/myapplication/Navigate.java
@@ -2,6 +2,8 @@ package com.example.android.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.RadioButton;
 
 /**
  * Created by aditya on 30/3/18.
@@ -15,5 +17,53 @@ public class Navigate extends AppCompatActivity{
         setContentView(R.layout.navigate);
 
 
+    }
+    String vehicle = null;
+    String lat = null;
+    String lon = null;
+
+    public void onvehicle(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.bike:
+                if (checked)
+                    vehicle = "bike";
+                    break;
+            case R.id.car:
+                if (checked)
+                    vehicle = "car";
+                    break;
+            case R.id.bus:
+                if (checked)
+                    vehicle = "bus";
+                    break;
+        }
+    }
+
+    public void onlocation(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.lat1:
+                if (checked)
+                    lat = "";
+                    lon = "";
+                    break;
+            case R.id.lat2:
+                if (checked)
+                    lat = "";
+                    lon = "";
+                    break;
+            case R.id.lat3:
+                if (checked)
+                    lat = "";
+                    lon = "";
+                    break;
+        }
     }
 }
